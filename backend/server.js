ab8b38ff5c30832e5b9c6021dddca505a9a65c8e
jamila704 <igguijamila67@gmail.com>
2026-04-13 23:35:38 +0100

    feat: docker compose fonctionnel - api et frontend operationnels

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Route de test
+app.get('/', (req, res) => {
+    res.json({ 
+        message: '✅ API E-commerce Analytics fonctionne !',
+        status: 'OK'
+    });
+});
+
+// Lance le serveur
+app.listen(PORT, () => {
+    console.log(`🚀 Serveur démarré sur le port ${PORT}`);
+});
